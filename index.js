e36948e1dd116917fdf8b00ce250ccc1e95d469b
joooons <joon.s.chang@gmail.com>
2020-07-22 16:56:58 -0700

    cloned starter proj from heroku

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const app = express();
-const http = require('http');
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-http.createServer(app).listen(3000);
-
-app.get('/', (req,res) => {
-    res.send('<div>Hello!</div>');
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
