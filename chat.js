bf2ef037401477db8ea52e3077498b6bf7be6f13
joooons <joon.s.chang@gmail.com>
2020-07-23 22:59:27 -0700

    fixed broadcast to io.emit. removed extra stuff

--- chat.js
+++
@@ -1,3 +0,0 @@
-
-
-console.log('from the chat.js');
--- index.js
+++ index.js
@@ -17,6 +17,6 @@ const io = socketIO(server);
 
 io.on('connection', (socket) => {
   socket.on('message', data => {
-    socket.broadcast.emit('message', data);
+    io.emit('message', data);
   });
 });
