19ef199a022ed80161113406c120ff87ec490457
joooons <joon.s.chang@gmail.com>
2020-07-23 20:32:32 -0700

    server works. no socket.io yet.

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
-const cool = require('cool-ascii-faces');
+
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+
+
 const express = require('express');
-const path = require('path');
-const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use( (req,res) => {
+  res.sendFile( INDEX, { root: __dirname } );
+});
+
+app.listen( PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
 
 
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.render( cool() ) )
-  .get('/times', (req,res) => res.send(showTimes()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  showTimes = () => {
-    let result = '';
-    const times = process.env.TIMES || 5;
-    for (i=0 ; i<times; i++ ) {
-      result += i + ' ';
-    }
-    return result;
-  }
