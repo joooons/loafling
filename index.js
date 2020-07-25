68c06971b6ad72b6ab8aeb3e2e93df9e37e1516f
joooons <joon.s.chang@gmail.com>
2020-07-25 14:08:05 -0700

    names move to rooms

--- index.js
+++ index.js
@@ -13,37 +13,85 @@ const io = socketIO(server);
 
 
 
-var lobbyNameMap = new Map();
+// local variables __________________________
+
+var ID_Name = new Map();
+  // Map { socket.id , name }
+
+var Name_Room = new Map();
+  // Map { name , room }
 
 
 
+
+
+
+// LOCAL FUNCTIONS _________________________________
+
+function MapToArray( map, keyStr, valStr ) {
+  let arr = []; 
+    map.forEach( (val, key) => {
+      let obj = {};
+      obj[keyStr] = key;
+      obj[valStr] = val;
+      arr.push( obj );
+    });
+  return arr;
+}
+
+
+
+
+
+
+
+
+
+
+
+// THE SOCKET.IO ENVIRONMENT __________________
 
 io.on('connection', (socket) => {
+  
+  console.log('--------------------------');
+  console.log('A user connected. But who?');
 
+
+  // DISCONNECT
   socket.on('disconnect', () => {
-    lobbyNameMap.delete(socket.id);
-    let arr = [];
-    lobbyNameMap.forEach( (val, key) => {
-      let obj = { id: 'none', name: 'none' }
-      obj.id = key;
-      obj.name = val;
-      arr.push(obj);
-    });
-    io.emit('new-user', arr);
+    let name = ID_Name.get(socket.id);
+    console.log(`${name} disconnected. Boo...`);
+
+    Name_Room.delete(name);
+    ID_Name.delete(socket.id);
+
+    let arr = MapToArray(Name_Room, "name", "room");
+    io.emit('update names', arr);
+  
   });
 
 
-  socket.on('new-user', data => {
-    let arr = [];
-    lobbyNameMap.set(socket.id, data);
-    lobbyNameMap.forEach( (val, key) => {
-      let obj = { id: 'none', name: 'none' }
-      obj.id = key;
-      obj.name = val;
-      arr.push(obj);
-    });
-    io.emit('new-user', arr);
+
+  // WHEN NEW USER ENTERS
+  socket.on('new user', name => {
+    ID_Name.set(socket.id, name);
+    Name_Room.set(name, 'room0');
+    console.log(`${name} connected!`);
+
+    let arr = MapToArray( Name_Room, "name", "room");
+    io.emit('update names', arr);
   });
+
+
+  // WHEN USER JOINS A ROOM
+  socket.on('join room', roomID => {
+    let name = ID_Name.get(socket.id);
+    Name_Room.set(name, roomID);
+
+    let arr = MapToArray( Name_Room, "name", "room");
+    io.emit('update names', arr);
+  });
+
 
 
 });
--- public/chat.js
+++ public/chat.js
@@ -3,35 +3,92 @@ const socket = io();
 
 
 
+// LOCAL VARIABLES ________________________________________
 
 var name = 'none';
+var room = 'room0';
+var numOfRooms = 4;
 
 
 
+
+
+// DOM ELEMENTS ___________________________________________
 
 const modal = document.querySelector('#modal');
 const pickName = document.querySelector('#pick-name');
-const lobbyUsers = document.querySelector('#lobbyUsers');
+
+const lobbySpace = document.querySelector('#lobbySpace');
+const roomSpace = document.querySelector('#roomSpace');
+const boardSpace = document.querySelector('#boardSpace');
 
 
 
-pickName.onchange = () => {
-    name = pickName.value;
-    modal.style.display = "none";
-    socket.emit('new-user', name);
+
+
+
+// LOCAL FUNCTIONS ____________________________________________
+
+makeRooms( numOfRooms );
+function makeRooms(num) {
+    for ( i=1 ; i<=num ; i++ ) {
+        let elem = document.createElement('div');
+        $(elem).append(`<b>Room ${i}</b>`);
+        let button = document.createElement('button');
+            let roomID = `room${i}`;
+            button.innerText = 'JOIN';
+            button.onclick = () => {
+                room = roomID;
+                socket.emit('join room', roomID);
+            }
+        $(elem).append(button);
+        $(elem).append(`<div id='room${i}'></div>`);
+        $(roomSpace).append(elem);
+    }
 }
 
 
 
-socket.on('new-user', data => {
-    let str = '';
-    data.forEach( val => {
-        str += `<div>${val.name}</div>`;
+
+
+
+// EVENT HANDLERS ________________________________________
+
+pickName.onchange = () => {
+    name = pickName.value;
+    modal.style.display = "none";
+    socket.emit('new user', name);
+}
+
+
+
+lobbySpace.onclick = () => {
+    room = 'room0';
+    socket.emit('join room', room);
+}
+
+
+
+
+
+
+
+
+
+
+
+// SOCKET EVENTS __________________________________________
+
+socket.on('update names', data => {
+    for ( i=0 ; i<=numOfRooms ; i++ ) {
+        $(`div[id="room${i}"]`).html('');
+    }
+
+    data.forEach( obj => {
+        let str = obj.name;
+        if (name == obj.name) str = `<b>${name}</b>`;
+        $(`div[id="${obj.room}"]`).append(`<div>${str}</div>`);
     });
-    lobbyUsers.innerHTML = str;
 });
-
-
-
 
 
